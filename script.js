51f2e7600e989e5719d5af07088f7b2a3f0dc7d3
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-11-11 14:56:00 +0100

    Rectifiction du JS et CSS pour appel toggle des class dans le JS

--- script.js
+++ script.js
@@ -3,28 +3,20 @@
 //? BURGER
 // Je stocke mes éléments
 const burger = document.getElementById('burger');
+const burgerBarre1 = document.getElementById('barre_1');
 const burgerBarre2 = document.getElementById('barre_2');
+const burgerBarre3 = document.getElementById('barre_3');
 
-// Je récupère mes éléments en console +> ok
+// Je récupère mes éléments en console => ok
 // console.log(burger);
 // console.log(burgerBarre1);
 // console.log(burgerBarre2);
 // console.log(burgerBarre3);
 
-// Au click ma barre2 disparait
+// Au click, j'affecte mes classes CSS et switch à chaque click
 burger.addEventListener('click', burgerBarres);
-// Au click les barres 1 fait une rotation
 function burgerBarres () {
-    //! Le stockage des ID ne fonctionne pas sauf pour burgerBarre2
-    // Barre du dessus => rotate décale la barre en bas sur l'axe Y => utilisation de translate pour la garder sur le même plan
-    document.getElementById('barre_1').style.transform = "translateY(10px) rotate(45deg)";
-    document.getElementById('barre_1').style.opacity = "1";
-    document.getElementById('barre_1').style.transition = "1s";
-    // Barre du milieu
-    burgerBarre2.style.opacity = '0';
-    burgerBarre2.style.transition = '1s';
-    // Barre du dessous => rotate décale la barre en bas sur l'axe Y => utilisation de translate pour la garder sur le même plan
-    document.getElementById('barre_3').style.transform = "translateY(-10px) rotate(-45deg)";
-    document.getElementById('barre_3').style.opacity = "1";
-    document.getElementById('barre_3').style.transition = "1s";
+    burgerBarre2.classList.toggle('opacity');
+    burgerBarre3.classList.toggle('rotate2');
+    burgerBarre1.classList.toggle('rotate1');
 };
