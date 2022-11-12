5b876c61935d52088ffa2eac800b4f5a199a3720
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-11-12 23:37:34 +0100

    Début du caroussel soutiens

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function burgerBarres() {
     burgerBarre1.classList.toggle('rotate1');
     // Animation du menu
     menu.classList.toggle('nav__mobile--show');
-}
+};
 
 // Essais de récupération des éléments en console => ok
 // console.log(burger);
@@ -51,8 +51,7 @@ const suivant = document.getElementById('temoignage_suivant');
 
 // Le temoignage 02 doit être visible par défaut => Les 2 autres doivent être en class displayoff
 // Un listener au click sur l'icône "précédent" qui appelle une fonction "temoignagePrecedent"
-// Un listener au click sur l'icône "suivant" qui appelle une fonction "temoignageSuivant"
-// ces fonctions suivant le temoignage affiché, détermine celui à afficher au click et ceux à cacher
+// ces fonctions suivant le temoignage affiché, détermine celui à suivre au click et ceux à cacher
 precedent.addEventListener('click', temoignagePrecedent);
 function temoignagePrecedent() {
     if (temoignage2.classList.contains('displayon')) {
@@ -70,8 +69,10 @@ function temoignagePrecedent() {
         temoignage2.classList.add('displayon');
         numero2.classList.add('gras');
     }
-}
+};
 
+// Un listener au click sur l'icône "suivant" qui appelle une fonction "temoignageSuivant"
+// ces fonctions suivant le temoignage affiché, détermine celui à suivre au click et ceux à cacher
 suivant.addEventListener('click', temoignageSuivant);
 function temoignageSuivant() {
     if (temoignage2.classList.contains('displayon')) {
@@ -89,7 +90,7 @@ function temoignageSuivant() {
         temoignage2.classList.add('displayon');
         numero2.classList.add('gras');
     }
-}
+};
 
 // Idem au click direct sur les numéros des articles
 numero1.addEventListener('click', numeroSwitch1);
@@ -103,7 +104,7 @@ function numeroSwitch1() {
     temoignage3.classList.remove('displayon');
     temoignage3.classList.add('displayoff');
     numero3.classList.remove('gras');
-}
+};
 
 numero2.addEventListener('click', numeroSwitch2);
 function numeroSwitch2() {
@@ -116,7 +117,7 @@ function numeroSwitch2() {
     temoignage3.classList.remove('displayon');
     temoignage3.classList.add('displayoff');
     numero3.classList.remove('gras');
-}
+};
 
 numero3.addEventListener('click', numeroSwitch3);
 function numeroSwitch3() {
@@ -129,4 +130,6 @@ function numeroSwitch3() {
     temoignage2.classList.remove('displayon');
     temoignage2.classList.add('displayoff');
     numero2.classList.remove('gras');
-}
+};
+
+//? SLIDE ILS NOUS SOUTIENNENT => NAVIGATION AU CLICK
